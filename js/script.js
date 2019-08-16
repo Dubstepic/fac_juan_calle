b15ce109a4895d2b3284ea114510cc90fd5d4bed
Dubstepic <JC@Dubstepics-MacBook-Air.local>
2019-08-16 19:53:23 +0100

    Added Scroll Navigation features that highlight the nav links depending on the section shown in the viewport

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+/* jshint esversion:6 */
 
+/* NavBar Scrolling. Highlights the link of the section shown on the viewport by adding
+and removing the 'active' class to the nav links*/
+let links = document.querySelectorAll('nav ul li a');
+window.addEventListener('scroll', event => {
+  let fromTop = window.scrollY;
+  links.forEach(link => {
+    let section = document.querySelector(link.hash);
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+});
